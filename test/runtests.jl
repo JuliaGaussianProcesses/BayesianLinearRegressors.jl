76e2be3eec9ea4970235eed9a8778a2854986615
Will Tebbutt <wt0881@my.bristol.ac.uk>
2020-02-28 14:22:11 +0000

    Fix some bit-rot

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
-using BayesianLinearRegressors, Distributions, LinearAlgebra, Random, Test, Zygote, FDM
+using BayesianLinearRegressors, Distributions, LinearAlgebra, Random, Test, Zygote, FiniteDifferences
 using BayesianLinearRegressors: BayesianLinearRegressor, posterior, marginals, cov, mean
-using FDM: j′vp
+using FiniteDifferences: j′vp
 
 @testset "BayesianLinearRegressors" begin
     include("bayesian_linear_regression.jl")
