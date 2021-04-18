0a3818d0b09a2c60e984c8a507f6cedc1558963e
WT <wt0881@my.bristol.ac.uk>
2021-04-18 16:37:49 +0100

    Remove redundant Zygote rule'

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+using AbstractGPs
 using BayesianLinearRegressors
 using Distributions
 using FiniteDifferences
@@ -8,9 +9,6 @@ using Zygote
 
 using BayesianLinearRegressors: BayesianLinearRegressor, posterior, marginals, cov, mean
 using FiniteDifferences: j′vp
-
-# Hack to make sure that we can ignore random number generation when computing derivatives.
-Zygote.@nograd MersenneTwister
 
 @testset "BayesianLinearRegressors" begin
     include("bayesian_linear_regression.jl")
