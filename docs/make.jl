43027410852f901cf39ef0a2b5d26cc267d874fc
Ross Viljoen <ross@viljoen.co.uk>
2022-01-08 20:57:57 +0000

    Run JuliaFormatter and add CI (#27)
    * Run JuliaFormatter and add CI
    
    * Fix some URLs
    
    Co-authored-by: willtebbutt <wct23@cam.ac.uk>

--- docs/make.jl
+++ docs/make.jl
@@ -3,15 +3,11 @@ using Documenter, BayesianLinearRegressors
 makedocs(;
     modules=[BayesianLinearRegressors],
     format=Documenter.HTML(),
-    pages=[
-        "Home" => "index.md",
-    ],
-    repo="https://github.com/willtebbutt/BayesianLinearRegressors.jl/blob/{commit}{path}#L{line}",
+    pages=["Home" => "index.md"],
+    repo="https://github.com/JuliaGaussianProcesses/BayesianLinearRegressors.jl/blob/{commit}{path}#L{line}",
     sitename="BayesianLinearRegressors.jl",
     authors="Will Tebbutt <wt0881@my.bristol.ac.uk>",
     assets=String[],
 )
 
-deploydocs(;
-    repo="github.com/willtebbutt/BayesianLinearRegressors.jl",
-)
+deploydocs(; repo="github.com/JuliaGaussianProcesses/BayesianLinearRegressors.jl")
